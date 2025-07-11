04c97be438e4f8cbf785076f4335005a8d85b961
git-abhishekyadav <abhishekyadav28@outlook.com>
2025-07-11 22:46:50 +0530

    fix: register form

--- apps/frontend/src/app/auth/pages/register/register.ts
+++ apps/frontend/src/app/auth/pages/register/register.ts
@@ -13,40 +13,39 @@ import { CommonModule } from '@angular/common';
 })
 export class Register implements AfterViewInit{
 
-  // private fb = inject(FormBuilder);
-
-  // registerForm: FormGroup =  this.fb.group({
-  //   email: ['', [Validators.required, Validators.email]],
-  //   username: [''],
-  //   password: ['', [Validators.required, Validators.minLength(6)]],
-  //   firstName: [''],
-  //   lastName: [''],
-  //   title: [''],
-  //   phone: [''],
-  //   address: [''],
-  //   apt: [''],
-  //   city: [''],
-  //   state: [''],
-  //   zipCode: [''],
-  //   country: [''],
-  //   superAdmin: [false],
-  //   role: [1]
-  // });
-
-
-
-  // onSubmit() {    
-  //   if (this.registerForm.valid) {
-  //     // Handle successful registration logic here
-  //     console.log('Registration successful', this.registerForm.value);
-  //   } else {
-  //     // Handle form errors
-  //     console.log('Form is invalid', this.registerForm.errors);
-  //   }
-  // }
-
-   private fb = inject(FormBuilder);
+  private fb = inject(FormBuilder);
   private auth = inject(Auth);
+
+  registerForm: FormGroup =  this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    username: [''],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    firstName: [''],
+    lastName: [''],
+    title: [''],
+    phone: [''],
+    address: [''],
+    apt: [''],
+    city: [''],
+    state: [''],
+    zipCode: [''],
+    country: [''],
+    superAdmin: [false],
+    role: [1]
+  });
+
+
+
+  onSubmit() {    
+    if (this.registerForm.valid) {
+      // Handle successful registration logic here
+      console.log('Registration successful', this.registerForm.value);
+    } else {
+      // Handle form errors
+      console.log('Form is invalid', this.registerForm.errors);
+    }
+  }
+
   @ViewChild('recaptchaContainer', { static: false }) recaptchaContainer: ElementRef = inject(ElementRef);
   phoneForm: FormGroup;
   otpSent = false;
@@ -101,7 +100,7 @@ recaptchaVerifier?: RecaptchaVerifier;
     try {
       const result = await signInWithPhoneNumber(
         this.auth,
-        '+917040307097',
+        this.phoneForm.value.phone,
         this.recaptchaVerifier
       );
       this.verificationId = result.verificationId;
