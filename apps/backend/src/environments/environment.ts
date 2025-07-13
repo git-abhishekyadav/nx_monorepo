68d6d5113467e966cfca6bb8257f07775a54e4f2
git-abhishekyadav <abhishekyadav28@outlook.com>
2025-07-13 23:20:43 +0530

    fix: firebase

--- apps/backend/src/environments/environment.ts
+++ apps/backend/src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
     'mongodb+srv://abhishekyadav28:5j8yw5FIpaQpdb2D@mean.32tozhj.mongodb.net/?retryWrites=true&w=majority&appName=mean',
   whiteList: ['http://localhost:4200', 'https://phcentral-v2-dev.innate.ly', 'https://api-v2-dev.innate.ly'],
   HORSE_OWNER_URL: 'http://localhost:4000/1/showday/horseHistory',
+  jwtSecret: '5j8yw5FIpaQpdb2D28vadaykehsihbayotisHesroh',
 
   frontEndUrl: 'http://localhost:4200',
   checkout: {
